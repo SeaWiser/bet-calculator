5e74b8aeb9a789b6a7991e1ee7f7684ffd89cee1
= <yaya.zabouche@gmail.com>
2021-05-29 14:53:01 +0200

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ function calculerMise() {
     let bankroll = document.getElementById('bankroll').value;
     let cote = document.getElementById('cote').value;
     let result = (bankroll * 0.1) * (0.3 / (cote - 1));
-    console.log(result);
-    // document.getElementById('result').value = Math.round(result) . '€';
     document.getElementById('result').value = Math.round((result + Number.EPSILON) * 100) / 100;
 }
